842093262d7a9fe85a8e2ee431cada9e99866388
Pozitronik <antikillerxxl@gmail.com>
2022-03-30 19:51:08 +0300

    Миграция используемого модуля

--- tests/_support/Extensions/DatabaseMigrationExtension.php
+++ tests/_support/Extensions/DatabaseMigrationExtension.php
@@ -21,5 +21,6 @@ class DatabaseMigrationExtension extends Extension {
 		$alias = __DIR__.'/../../_app/yii';
 		$cli->runShellCommand("php $alias migrate/up --interactive=0");
 		$cli->runShellCommand("php $alias migrate/up --migrationPath=./migrations --interactive=0");
+		$cli->runShellCommand("php $alias migrate/up --migrationPath=@vendor/pozitronik/yii2-users-options/migrations --interactive=0");
 	}
 }
