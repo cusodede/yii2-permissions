fefc9d802fd7b630a4761f216b104590e8d799cb
Pozitronik <antikillerxxl@gmail.com>
2022-03-30 18:27:05 +0300

    Минимальный тестовый тест

--- tests/functional/controllers/SiteControllerCest.php
+++ tests/functional/controllers/SiteControllerCest.php
@@ -1,18 +1,20 @@
 <?php
+declare(strict_types = 1);
+
 namespace controllers;
 
 use FunctionalTester;
-use yii\helpers\VarDumper;
 
-class SiteControllerCest
-{
-    // tests
-    public function actionIndex(FunctionalTester $I)
-    {
-        $I->amOnPage('/site/index');
-        $I->seeResponseCodeIs(200);
-        $I->seeResponseContainsJson([
-            'result' => 1
-        ]);
-    }
+/**
+ * Class SiteControllerCest
+ */
+class SiteControllerCest {
+	// tests
+	public function actionIndex(FunctionalTester $I) {
+		$I->amOnPage('/site/index');
+		$I->seeResponseCodeIs(200);
+		$I->seeResponseContainsJson([
+			'result' => 1
+		]);
+	}
 }
