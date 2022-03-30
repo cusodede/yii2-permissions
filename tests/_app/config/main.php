8c76f6217232d8edb7a068fefbdff737886d609e
Pozitronik <antikillerxxl@gmail.com>
2022-03-30 16:50:30 +0300

    Конфиг для модуля

--- tests/_app/config/main.php
+++ tests/_app/config/main.php
@@ -1,52 +1,54 @@
 <?php
+declare(strict_types = 1);
+
+use yii\log\FileTarget;
+use yii\caching\DummyCache;
+
+$permissions = require __DIR__.'/permissions.php';
 
 $config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-    ],
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Aotbwxo1MpyD4vplOqZcdY62xmLvNSTg',
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-    ],
-    'params' => [],
+	'id' => 'basic',
+	'basePath' => dirname(__DIR__),
+	'bootstrap' => ['log'],
+	'aliases' => [
+		'@bower' => '@vendor/bower-asset',
+		'@npm' => '@vendor/npm-asset',
+	],
+	'modules' => [
+		'permissions' => $permissions
+	],
+	'components' => [
+		'request' => [
+			// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+			'cookieValidationKey' => 'sosijopu',
+		],
+		'cache' => [
+			'class' => DummyCache::class,
+		],
+		'user' => [
+			'identityClass' => 'app\models\User',
+			'enableAutoLogin' => true,
+		],
+		'errorHandler' => [
+			'errorAction' => 'site/error',
+		],
+		'log' => [
+			'traceLevel' => YII_DEBUG?3:0,
+			'targets' => [
+				[
+					'class' => FileTarget::class,
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName' => false,
+			'rules' => [
+			],
+		],
+	],
+	'params' => [],
 ];
 
 return $config;
