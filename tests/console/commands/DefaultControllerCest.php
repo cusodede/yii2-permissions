1f3c870d16326dbf4e131f2abf1f340d0e673b23
Pozitronik <antikillerxxl@gmail.com>
2022-03-31 13:07:34 +0300

    Дописал консольные тесты

--- tests/console/commands/DefaultControllerCest.php
+++ tests/console/commands/DefaultControllerCest.php
@@ -3,13 +3,16 @@ declare(strict_types = 1);
 
 namespace console\commands;
 
+use app\models\Users;
 use ConsoleTester;
 use cusodede\permissions\commands\DefaultController;
 use cusodede\permissions\models\Permissions;
 use cusodede\permissions\models\PermissionsCollections;
+use Throwable;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\console\Controller;
+use yii\db\Exception;
 
 /**
  * Class DefaultControllerCest
@@ -26,6 +29,14 @@ class DefaultControllerCest {
 	}
 
 	/**
+	 * @return Users
+	 * @throws Exception
+	 */
+	private function initUser():Users {
+		return Users::CreateUser()->saveAndReturn();
+	}
+
+	/**
 	 * Проверяем корректность команды отработки генератора доступов по конфигу
 	 * @param ConsoleTester $I
 	 * @return void
@@ -34,8 +45,45 @@ class DefaultControllerCest {
 	public function InitControllerPermissionsFromConfig(ConsoleTester $I):void {
 		/*Генерирует доступы для всех контроллеров в конфиге: три контроллера в tests/_app/controllers и два - в /src/controllers */
 		$this->initDefaultController()->actionInitControllersPermissions();
-		$I->assertCount(29, Permissions::find()->all());
-		$I->assertCount(5, PermissionsCollections::find()->all());
+		$allPermissions = Permissions::find()->all();
+		$allPermissionsCollections = PermissionsCollections::find()->all();
+		$I->assertCount(29, $allPermissions);
+		$I->assertCount(5, $allPermissionsCollections);
+
+		$user = $this->initUser();
+		$user->setRelatedPermissions($allPermissions);
+		$user->save();
+
+		/*Потыкаем в разные сгенеренные пермиссии*/
+		$I->assertTrue($user->hasPermission([
+			'permissions-collections:index', 'permissions:index', 'permissions:permissions-collections:edit', 'permissions:permissions:view',
+			'permissions-collections:create', 'permissions:ajax-search', 'site:error'
+		], Permissions::LOGIC_AND));
+
+		/*Потыкаем в доступы к контроллерам*/
+		$I->assertTrue($user->hasControllerPermission('permissions', 'index'));
+		$I->assertTrue($user->hasControllerPermission('site', 'error'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'GET'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'POST'));
+
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', null, 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions', 'ajax-search', 'POST', 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'edit', 'GET', 'permissions'));
+
+		/*Уберём пермиссии, добавим коллекции*/
+		$user->setRelatedPermissions([]);
+		$user->setRelatedPermissionsCollections($allPermissionsCollections);
+		$user->save();
+
+		/*Потыкаем в доступы к контроллерам снова*/
+		$I->assertTrue($user->hasControllerPermission('permissions', 'index'));
+		$I->assertTrue($user->hasControllerPermission('site', 'error'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'GET'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'POST'));
+
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', null, 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions', 'ajax-search', 'POST', 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'edit', 'GET', 'permissions'));
 	}
 
 	/**
@@ -46,8 +94,49 @@ class DefaultControllerCest {
 	 */
 	public function InitControllerPermissionsByPath(ConsoleTester $I):void {
 		$this->initDefaultController()->actionInitControllersPermissions('@app/controllers');
-		$I->assertCount(15, Permissions::find()->all());
-		$I->assertCount(3, PermissionsCollections::find()->all());
+
+		$allPermissions = Permissions::find()->all();
+		$allPermissionsCollections = PermissionsCollections::find()->all();
+		$I->assertCount(15, $allPermissions);
+		$I->assertCount(3, $allPermissionsCollections);
+
+		$user = $this->initUser();
+		$user->setRelatedPermissions($allPermissions);
+		$user->save();
+
+		/*Потыкаем в разные сгенеренные пермиссии*/
+		$I->assertTrue($user->hasPermission(
+			['permissions-collections:index', 'permissions:index', 'permissions-collections:create', 'permissions:ajax-search', 'site:error'],
+			Permissions::LOGIC_AND)
+		);
+
+		$I->assertFalse($user->hasPermission(['permissions:permissions-collections:edit', 'permissions:permissions:view'], Permissions::LOGIC_AND));
+
+		/*Потыкаем в доступы к контроллерам*/
+		$I->assertTrue($user->hasControllerPermission('permissions', 'index'));
+		$I->assertTrue($user->hasControllerPermission('site', 'error'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'GET'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'POST'));
+
+		/*А контроллеров модуля нет*/
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'create', null, 'permissions'));
+		$I->assertFalse($user->hasControllerPermission('permissions', 'ajax-search', 'POST', 'permissions'));
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'edit', 'GET', 'permissions'));
+
+		/*Уберём пермиссии, добавим коллекции*/
+		$user->setRelatedPermissions([]);
+		$user->setRelatedPermissionsCollections($allPermissionsCollections);
+		$user->save();
+
+		/*Потыкаем в доступы к контроллерам снова*/
+		$I->assertTrue($user->hasControllerPermission('permissions', 'index'));
+		$I->assertTrue($user->hasControllerPermission('site', 'error'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'GET'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', 'POST'));
+
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'create', null, 'permissions'));
+		$I->assertFalse($user->hasControllerPermission('permissions', 'ajax-search', 'POST', 'permissions'));
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'edit', 'GET', 'permissions'));
 	}
 
 	/**
@@ -58,7 +147,73 @@ class DefaultControllerCest {
 	 */
 	public function InitControllerPermissionsByPathInModule(ConsoleTester $I):void {
 		$this->initDefaultController()->actionInitControllersPermissions('./src/controllers', 'permissions');
-		$I->assertCount(14, Permissions::find()->all());
-		$I->assertCount(2, PermissionsCollections::find()->all());
+
+		$allPermissions = Permissions::find()->all();
+		$allPermissionsCollections = PermissionsCollections::find()->all();
+		$I->assertCount(14, $allPermissions);
+		$I->assertCount(2, $allPermissionsCollections);
+
+		$user = $this->initUser();
+		$user->setRelatedPermissions($allPermissions);
+		$user->save();
+
+		/*Потыкаем в разные сгенеренные пермиссии*/
+		$I->assertFalse($user->hasPermission(
+			['permissions-collections:index', 'permissions:index', 'permissions-collections:create', 'permissions:ajax-search', 'site:error'],
+			Permissions::LOGIC_AND)
+		);
+
+		$I->assertTrue($user->hasPermission(['permissions:permissions-collections:edit', 'permissions:permissions:view'], Permissions::LOGIC_AND));
+
+		/*Потыкаем в доступы к контроллерам*/
+		$I->assertFalse($user->hasControllerPermission('permissions', 'index'));
+		$I->assertFalse($user->hasControllerPermission('site', 'error'));
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'create', 'GET'));
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'create', 'POST'));
+
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', null, 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions', 'ajax-search', 'POST', 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'edit', 'GET', 'permissions'));
+
+		/*Уберём пермиссии, добавим коллекции*/
+		$user->setRelatedPermissions([]);
+		$user->setRelatedPermissionsCollections($allPermissionsCollections);
+		$user->save();
+
+		/*Потыкаем в доступы к контроллерам снова*/
+		$I->assertFalse($user->hasControllerPermission('permissions', 'index'));
+		$I->assertFalse($user->hasControllerPermission('site', 'error'));
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'create', 'GET'));
+		$I->assertFalse($user->hasControllerPermission('permissions-collections', 'create', 'POST'));
+
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'create', null, 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions', 'ajax-search', 'POST', 'permissions'));
+		$I->assertTrue($user->hasControllerPermission('permissions-collections', 'edit', 'GET', 'permissions'));
+
+	}
+
+	/**
+	 * Проверяет присвоение и отработку назначения привилегий через конфиг
+	 * @param ConsoleTester $I
+	 * @return void
+	 * @throws Exception
+	 * @throws InvalidConfigException
+	 * @throws Throwable
+	 */
+	public function InitConfigPermissions(ConsoleTester $I) {
+		$user = $this->initUser();
+		$I->assertEquals(1, $user->id);
+		$this->initDefaultController()->actionInitConfigPermissions();
+		/*В конфиге у юзера прибит один один пермишшен*/
+		$I->assertCount(1, $user->allPermissions());
+
+		/*Доступ, которого нет*/
+		$I->assertFalse($user->hasPermission(['this_permission_does_not_exist']));
+
+		/*Доступ, который есть в конфиге, но не у юзера */
+		$I->assertFalse($user->hasPermission(['execute_order_66']));
+		/*Доступ есть в конфиге, и присвоен юзеру добавлен в конфиге*/
+		$I->assertTrue($user->hasPermission(['choke_with_force']));
+
 	}
 }
