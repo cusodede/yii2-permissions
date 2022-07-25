bd9860cc8efd34362b21027a9a28aaed81bb0bfc
Pozitronik <antikillerxxl@gmail.com>
2022-07-25 09:01:31 +0300

    #23 Тест переноса конфигурации доступов из файловых конфигураций в БД

--- tests/unit/PermissionsModuleTest.php
+++ tests/unit/PermissionsModuleTest.php
@@ -2,7 +2,9 @@
 declare(strict_types = 1);
 
 use Codeception\Test\Unit;
+use cusodede\permissions\models\Permissions;
 use cusodede\permissions\PermissionsModule;
+use yii\helpers\ArrayHelper;
 
 /**
  * Class PermissionsModuleTests
@@ -11,8 +13,21 @@ use cusodede\permissions\PermissionsModule;
 class PermissionsModuleTest extends Unit {
 
 	/**
+	 * Перезагружает модуль с указанными параметрами
+	 * @param array $params
+	 * @return PermissionsModule
+	 */
+	private static function ModuleWithParams(array $params):PermissionsModule {
+		$module = new PermissionsModule('permissions', null, [
+			'params' => $params
+		]);
+		Yii::$app->setModule('permissions', $module);
+		return $module;
+	}
+
+	/**
 	 * @return void
-	 * @see PermissionsModule::UserIdentityClass
+	 * @covers PermissionsModule::UserIdentityClass
 	 */
 	public function testUserIdentityClass():void {
 
@@ -20,7 +35,7 @@ class PermissionsModuleTest extends Unit {
 
 	/**
 	 * @return void
-	 * @see PermissionsModule::UserCurrentIdentity
+	 * @covers PermissionsModule::UserCurrentIdentity
 	 */
 	public function testUserCurrentIdentity():void {
 
@@ -28,7 +43,7 @@ class PermissionsModuleTest extends Unit {
 
 	/**
 	 * @return void
-	 * @see PermissionsModule::FindIdentityById
+	 * @covers PermissionsModule::FindIdentityById
 	 */
 	public function testFindIdentityById():void {
 
@@ -36,23 +51,48 @@ class PermissionsModuleTest extends Unit {
 
 	/**
 	 * @return void
-	 * @see PermissionsModule::GetControllersList
+	 * @covers PermissionsModule::GetControllersList
 	 */
 	public function testGetControllersList():void {
 
 	}
 
 	/**
+	 * Тест переноса конфигурации доступов из файловых конфигураций в БД
 	 * @return void
-	 * @see PermissionsModule::InitConfigPermissions
+	 * @covers PermissionsModule::InitConfigPermissions
 	 */
 	public function testInitConfigPermissions():void {
+		static::ModuleWithParams([
+			'permissions' => [
+				'test_permission_1' => [],
+				'test_permission_2' => [
+					'comment' => 'test_permission_2 comment'
+				],
+				'site:error:post' => [
+					'controller' => 'site',
+					'action' => 'error',
+					'verb' => 'post',
+					'comment' => 'Разрешение POST для actionError в SiteController'
+				]
+			]
+		]);
+
+		$this::assertEmpty(Permissions::find()->all());
+
+		/*Доступы переносятся в БД*/
+		PermissionsModule::InitConfigPermissions();
+
+		$this::assertCount(3, Permissions::find()->all());
+		$this::assertEquals(
+			['test_permission_2', 'test_permission_1', 'site:error:post'],
+			ArrayHelper::getColumn(Permissions::find()->select(['name'])->orderBy(['name' => SORT_DESC])->asArray(true)->all(), 'name'));
 
 	}
 
 	/**
 	 * @return void
-	 * @see PermissionsModule::InitControllersPermissions
+	 * @covers PermissionsModule::InitControllersPermissions
 	 */
 	public function testInitControllersPermissions():void {
 
