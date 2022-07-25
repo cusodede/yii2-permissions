4aedaeea63ae2cf1e3d4eab92ce742091c2ac265
Pozitronik <antikillerxxl@gmail.com>
2022-07-25 07:50:38 +0300

    #23 Стаб для тестов PermissionsModule

---
+++ tests/unit/PermissionsModuleTest.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types = 1);
+
+use Codeception\Test\Unit;
+use cusodede\permissions\PermissionsModule;
+
+/**
+ * Class PermissionsModuleTests
+ * Тесты методов модуля PermissionsTests
+ */
+class PermissionsModuleTest extends Unit {
+
+	/**
+	 * @return void
+	 * @see PermissionsModule::UserIdentityClass
+	 */
+	public function testUserIdentityClass():void {
+
+	}
+
+	/**
+	 * @return void
+	 * @see PermissionsModule::UserCurrentIdentity
+	 */
+	public function testUserCurrentIdentity():void {
+
+	}
+
+	/**
+	 * @return void
+	 * @see PermissionsModule::FindIdentityById
+	 */
+	public function testFindIdentityById():void {
+
+	}
+
+	/**
+	 * @return void
+	 * @see PermissionsModule::GetControllersList
+	 */
+	public function testGetControllersList():void {
+
+	}
+
+	/**
+	 * @return void
+	 * @see PermissionsModule::InitConfigPermissions
+	 */
+	public function testInitConfigPermissions():void {
+
+	}
+
+	/**
+	 * @return void
+	 * @see PermissionsModule::InitControllersPermissions
+	 */
+	public function testInitControllersPermissions():void {
+
+	}
+
+}
