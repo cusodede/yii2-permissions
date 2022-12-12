e4155e0facb9e2ab06430b6925625b2d40ba3460
Pozitronik <antikillerxxl@gmail.com>
2022-12-12 13:58:20 +0400

    issue #35 A method for checking controller path actuality

--- src/helpers/CommonHelper.php
+++ src/helpers/CommonHelper.php
@@ -3,6 +3,12 @@ declare(strict_types = 1);
 
 namespace cusodede\permissions\helpers;
 
+use pozitronik\helpers\ControllerHelper;
+use Throwable;
+use Yii;
+use yii\base\Controller;
+use yii\base\InvalidConfigException;
+
 /**
  * Class CommonHelper
  */
@@ -19,5 +25,23 @@ class CommonHelper {
 			$output[] = "{$attribute}: {$error}";
 		}
 		return implode($separator, $output);
+	}
+
+	/**
+	 * Checks, if module/controller/action path is exists
+	 * @param string|null $moduleId
+	 * @param string|null $controllerId
+	 * @param string|null $actionId
+	 * @return null|bool true/false: actuality of the checked path, null: it is not a controller permission
+	 * @throws Throwable
+	 * @throws InvalidConfigException
+	 */
+	public static function IsControllerPathExits(?string $moduleId, ?string $controllerId, ?string $actionId):?bool {
+		if (null === $controllerId) return null;
+		if (null !== $moduleId && !Yii::$app->hasModule($moduleId)) return false;
+		/** @var Controller|null $controller */
+		if (null === $controller = ControllerHelper::GetControllerByControllerId($controllerId, $actionId)) return false;
+		if (null === $controller->createAction($actionId)) return false;
+		return true;
 	}
 }
