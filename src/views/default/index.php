d27bb7a1bb3c1428a0f2534c4f98af76f848c281
Pozitronik <antikillerxxl@gmail.com>
2022-12-12 14:07:17 +0400

    issue #35 label fixed

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -15,6 +15,6 @@ use yii\web\View;
 	'buttons' => [
 		PermissionsModule::a('Добавляет разрешения, описанные в файле конфигурации', 'default/init-config-permissions', ['class' => 'btn btn-default']),
 		PermissionsModule::a('Генерация доступов по контроллерам', 'default/init-controllers-permissions', ['class' => 'btn btn-default']),
-		PermissionsModule::a('Генерация доступов по контроллерам', 'default/drop-controllers-permissions', ['class' => 'btn btn-warning']),
+		PermissionsModule::a('Удаление неиспользуемых контроллеров', 'default/drop-controllers-permissions', ['class' => 'btn btn-warning']),
 	]
 ]) ?>
