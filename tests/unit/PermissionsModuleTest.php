f836a00a1ab3477a674ade8bdb69d1d643ff13d4
Pozitronik <antikillerxxl@gmail.com>
2023-01-13 15:56:47 +0400

    #fix_unusual_constructors Tests + readme

--- tests/unit/PermissionsModuleTest.php
+++ tests/unit/PermissionsModuleTest.php
@@ -5,7 +5,10 @@ use Codeception\Test\Unit;
 use cusodede\permissions\models\Permissions;
 use cusodede\permissions\models\PermissionsCollections;
 use cusodede\permissions\PermissionsModule;
+use yii\base\InvalidConfigException;
 use yii\base\UnknownClassException;
+use yii\caching\DummyCache;
+use yii\caching\FileCache;
 use yii\web\Controller;
 
 /**
@@ -56,5 +59,51 @@ class PermissionsModuleTest extends Unit {
 		PermissionsModule::GenerateControllersPermissions([$controllerTwo]);
 		static::assertCount(2, Permissions::find()->all());
 		static::assertCount(1, PermissionsCollections::find()->all());
+	}
+
+
+	/**
+	 * @covers PermissionsModule::Cache
+	 * @return void
+	 * @throws Throwable
+	 * @throws InvalidConfigException
+	 */
+	public function testCacheDefault():void {
+		/* flush static storage variable */
+		$propertyReflection = new ReflectionProperty(PermissionsModule::class, '_cacheComponent');
+		$propertyReflection->setAccessible(true);
+		$propertyReflection->setValue(null);
+
+		Yii::$app->setModule('cache', [
+			'class' => DummyCache::class,
+		]);
+
+		static::assertInstanceOf(DummyCache::class, PermissionsModule::Cache());
+	}
+
+	/**
+	 * @covers PermissionsModule::Cache
+	 * @return void
+	 * @throws Throwable
+	 * @throws InvalidConfigException
+	 */
+	public function testCacheOverride():void {
+		/* flush static storage variable */
+		$propertyReflection = new ReflectionProperty(PermissionsModule::class, '_cacheComponent');
+		$propertyReflection->setAccessible(true);
+		$propertyReflection->setValue(null);
+
+		Yii::$app->setModule('cache', [
+			'class' => DummyCache::class,
+		]);
+
+		Yii::$app->setModule('permissions', [
+			'class' => PermissionsModule::class,
+			'params' => [
+				'cache' => FileCache::class
+			]
+		]);
+
+		static::assertInstanceOf(FileCache::class, PermissionsModule::Cache());
 	}
 }
