c4fd57447795a9a7d398669ec25c1b488b3215a0
Pozitronik <antikillerxxl@gmail.com>
2023-01-16 21:31:40 +0400

    issue #40 form and template fixes

--- src/generators/migration/default/permissions_migration.php
+++ src/generators/migration/default/permissions_migration.php
@@ -21,18 +21,18 @@ use app\components\db\Migration;
 * Class <?= $className."\n" ?>
 */
 class <?= $className ?> extends Migration {
-/**
-* {@inheritdoc}
-*/
-public function safeUp() {
+	/**
+	* {@inheritdoc}
+	*/
+	public function safeUp() {
 
-}
+	}
 
-/**
-* {@inheritdoc}
-*/
-public function safeDown() {
+	/**
+	* {@inheritdoc}
+	*/
+	public function safeDown() {
 
-}
+	}
 
 }
